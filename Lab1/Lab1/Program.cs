1c10adafb801fc41981a5f78738f1e48604f9ac3
MatiBa8 <mibustos2@miuandes.cl>
2020-03-17 20:34:20 -0300

    FirstCode

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lab1
+{
+    public class Person
+    {
+        //Atributos
+        public string Name;
+        public string LastName;
+
+        public void Lanzar()    //Comportamiento
+        {
+            Random rnd = new Random();
+            Console.WriteLine(rnd.Next(0, 3));
+        }
+
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Bob";
+            person.LastName = "Kunga";
+            
+            person.Lanzar();
+            
+        }
+    }
+
+    
+}
