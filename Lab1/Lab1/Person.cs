f3719ec18f5f61d70857f99d65508113206b84a0
MatiBa8 <mibustos2@miuandes.cl>
2020-03-19 13:37:33 -0300

    Final

---
+++ Lab1/Lab1/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab1
+{
+    public class Person
+    {
+        //Atributos
+        private string name;
+        private string LastName;
+
+        public Person(string name,string LastName)
+        {
+            this.name = name;
+            this.LastName = LastName;
+        }
+
+        public string GetName() 
+        {
+            return name;
+        }
+        public string GetLastName()
+        {
+            return LastName;
+        }
+        public void Lanzar()    //Comportamiento
+        {
+            Random rnd = new Random();
+            Console.WriteLine(rnd.Next(0, 3));
+
+        }
+
+
+    }
+
+    
+}
--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -2,32 +2,17 @@
 
 namespace Lab1
 {
-    public class Person
-    {
-        //Atributos
-        public string Name;
-        public string LastName;
-
-        public void Lanzar()    //Comportamiento
-        {
-            Random rnd = new Random();
-            Console.WriteLine(rnd.Next(0, 3));
-        }
-
-
-    }
-
-
     class Program
     {
         static void Main(string[] args)
         {
-            Person person = new Person();
-            person.Name = "Bob";
-            person.LastName = "Kunga";
-            
-            person.Lanzar();
-            
+            Person p1 = new Person("Bob", "Konga");
+            p1.Lanzar();
+           
+
+       
+
+
         }
     }
 
